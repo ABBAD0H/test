db688f9cb2532fbf16618e2a784eda45048d9e85
ABBAD0H <148543844+ABBAD0H@users.noreply.github.com>
2023-10-20 14:11:34 +0300

    Update main.cpp

--- test_3d_figures/ConsoleApplication1/main.cpp
+++ test_3d_figures/ConsoleApplication1/main.cpp
@@ -5,12 +5,12 @@
 #include <omp.h>
 #include "figure.h"
 #define pi 3.1415926535
-using namespace std;
+
 
 void main()
 {
 	srand(time(NULL));
-	vector<circle*> box;			//Generating shapes
+	std::vector<circle*> box;			//Генерация фигур
 	for (int i = 0; i < 10; i++)
 	{
 		unsigned short choise = rand() % 3 + 1;
@@ -48,11 +48,11 @@ void main()
 		}
 	}
 
-	for (auto it = box.begin(); it != box.end(); it++)		//output of vectors and points at t=pi/4
+	for (auto it = box.begin(); it != box.end(); it++)		//вывод векторов и точек при t=pi/4
 		(*it)->send((pi / 4));
 
 
-	vector<circle*> circle_box;								//Filtering circles
+	std::vector<circle*> circle_box;								//Фильтрация по кругам
 	for (auto it = box.begin(); it != box.end(); it++) 
 	{
 		if (typeid(**it) == typeid(circle)) 
@@ -61,22 +61,20 @@ void main()
 		}
 	}
 	
-	sort(circle_box.begin(), circle_box.end(), [](const circle* p1, const circle* p2) //sorting by radii
+	sort(circle_box.begin(), circle_box.end(), [](const circle* p1, const circle* p2) //сортировка по радиусам
 	{
 		return p1->r < p2->r;
 	});
 
 
 
-	float radius_sum = 0;									//Calculating the sum of radii
+	float radius_sum = 0;									//Подсчет суммы радиусов
 	#pragma omp parallel for reduction(+:radius_sum)
 	for (int i = 0; i < circle_box.size(); i++)
 	{
 		radius_sum += circle_box[i]->r;
 	}
-	spring test(0, 0, 1,(1/(2*pi)));
-	space_point aa = test.point(0);
-	aa = test.point((2 * pi));
+
 	box.clear();
 	circle_box.clear();
 	return;
